bcca0a88a2428d11abc613794b853316a5a5bd01
lexdenotter <lexdenotter@home.nl>
2016-11-09 15:03:40 +0100

    fix naar pin in var

--- GUI.py
+++ GUI.py
@@ -13,30 +13,18 @@ root.configure(background='#66ccff')
 userinput = StringVar()
 myEntry = Entry(root, textvariable=userinput).pack()
 
-file = open('pincodefile.txt', 'r+')
-pincode = file.readline()
-
 # Functies
-#pincode inlezen uit bestand en opslaan in de var pincode
-with open('pincodefile.txt', 'r+') as bestand:
-    pin = bestand.read()
-    if bestand == '':
-        bestand.write('0000')
-
+pincode = 0000
 def nieuwepincode():
     nwpincode = random.randrange(1000,9999)
     global pincode
     pincode = nwpincode
     Label(root,text='Je nieuwe pincode is: {}'.format(nwpincode)).pack()
-    #code om pincode aan te passen
-    file.flush()
-    file.writelines(str(nwpincode))
-    file.close()
 
 def pincodefunct():
     try:
         userinputpincode = userinput.get()
-        if pincode == str(userinputpincode):
+        if pincode == eval(userinputpincode):
             Label(root,text='Correct!').pack()
         else:
             Label(root,text='Helaas fout, probeer opnieuw!').pack()
@@ -44,8 +32,6 @@ def pincodefunct():
         print('fout')
 
 # Buttons
-
-
 okbutton = Button(root, text='OK', command=pincodefunct,fg='red', bg='blue').pack()
 pincodebutton = Button(root, text='Nieuwe pincode!', command=nieuwepincode, fg='red', bg='blue').pack()
 
